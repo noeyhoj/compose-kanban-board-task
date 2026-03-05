5d37b1f137fce5e9e5f0bafb51d47ac1c0f19135
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-05 21:12:06 +0900

    feat: 보드의 사용된 색상과 보드에 들어갈 내용과 같은 중복되는 내용을 상수로 관리하기 위해 파일을 만듦

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/CustomConst.kt
@@ -0,0 +1,16 @@
+package woowacourse.kanban.board.model
+
+// 보드에 사용된 색상
+const val TAG_COLOR = 0xffF3F4F6
+const val PROFILE_BG_COLOR = 0xFF888888
+const val PROFILE_COLOR = 0xFFFFFFFF
+const val BORDER_COLOR = 0xffE5E7EB
+const val CONTENT_COLOR = 0xff4A5565
+
+// 보드에 들어갈 내용
+const val DEFAULT_TITLE = "LazyColumn 컴포넌트 구현"
+const val MAX_TITLE = "너무너무 긴 제목은 한 줄까지만 노출합니다."
+const val DEFAULT_CONTENT = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."
+const val MAX_CONTENT = "너무너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다 두 줄까지만 노출합니다."
+const val DEFAULT_NAME = "다이노"
+const val MAX_NAME = "너무너무너무 긴 담당자도 한 줄까지만 노출합니다."
