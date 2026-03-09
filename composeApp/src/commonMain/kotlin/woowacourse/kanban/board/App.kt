31ae17c0c82e8db03a6c071cde3943b0e6dfea21
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-09 13:52:39 +0900

    feat: App.kt에 보드를 볼 수 있도록 기능을 추가함

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,7 +1,65 @@
 package woowacourse.kanban.board
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.component.KanbanBoardTemplate
+import woowacourse.kanban.board.constant.DEFAULT_CONTENT
+import woowacourse.kanban.board.constant.DEFAULT_NAME
+import woowacourse.kanban.board.constant.DEFAULT_TITLE
+import woowacourse.kanban.board.constant.MAX_CONTENT
+import woowacourse.kanban.board.constant.MAX_NAME
+import woowacourse.kanban.board.constant.MAX_TITLE
+import woowacourse.kanban.board.model.BoardData
+import woowacourse.kanban.board.model.Nickname
+import woowacourse.kanban.board.model.Tags
+import woowacourse.kanban.board.model.Title
 
 @Composable
 fun App() {
+    val values = listOf(
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            content = DEFAULT_CONTENT,
+            tags = Tags(listOf("컴포넌트", "성능")),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            tags = Tags(listOf("컴포넌트", "성능")),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            content = DEFAULT_CONTENT,
+            tags = Tags(),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            tags = Tags(),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(MAX_TITLE),
+            content = MAX_CONTENT,
+            tags = Tags(listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임")),
+            nickname = Nickname(MAX_NAME),
+        ),
+    )
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        items(values) { item ->
+            KanbanBoardTemplate(
+                title = item.title,
+                content = item.content,
+                tags = item.tags,
+                nickname = item.nickname,
+            )
+        }
+    }
 }
