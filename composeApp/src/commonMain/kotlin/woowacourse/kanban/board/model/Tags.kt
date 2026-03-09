9ea02a44cb4573eaa8f45187168b9d2a2d41c737
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-09 12:02:58 +0900

    refactor: 중복되는 에러 테그에 대해 상수화로 리펙토링함

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tags.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tags.kt
@@ -1,7 +1,9 @@
 package woowacourse.kanban.board.model
 
+import woowacourse.kanban.board.constant.ERROR_TAG
+
 class Tags(val tags: List<String> = listOf()) {
     init {
-        require(tags.size <= 5) { throw IllegalArgumentException("[ERROR] 테그의 개수는 5개 이하로 작성해야 합니다.") }
+        require(tags.size <= 5) { throw IllegalArgumentException("$ERROR_TAG 테그의 개수는 5개 이하로 작성해야 합니다.") }
     }
 }
