a197cdf74b81fb1de5989a0c254b76bba675f5e7
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-09 13:41:44 +0900

    style: PreviewParameter를 Perview 컴포넌트가 있는 곳으로 옮김

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,65 +1,7 @@
 package woowacourse.kanban.board
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import woowacourse.kanban.board.component.KanbanBoardTemplate
-import woowacourse.kanban.board.constant.DEFAULT_CONTENT
-import woowacourse.kanban.board.constant.DEFAULT_NAME
-import woowacourse.kanban.board.constant.DEFAULT_TITLE
-import woowacourse.kanban.board.constant.MAX_CONTENT
-import woowacourse.kanban.board.constant.MAX_NAME
-import woowacourse.kanban.board.constant.MAX_TITLE
-import woowacourse.kanban.board.model.BoardData
-import woowacourse.kanban.board.model.Nickname
-import woowacourse.kanban.board.model.Tags
-import woowacourse.kanban.board.model.Title
 
 @Composable
 fun App() {
-}
-
-class BoardPreviewParameterProvider : PreviewParameterProvider<BoardData> {
-    override val values = sequenceOf(
-        BoardData(
-            title = Title(DEFAULT_TITLE),
-            content = DEFAULT_CONTENT,
-            tags = Tags(listOf("컴포넌트", "성능")),
-            nickname = Nickname(DEFAULT_NAME),
-        ),
-        BoardData(
-            title = Title(DEFAULT_TITLE),
-            tags = Tags(listOf("컴포넌트", "성능")),
-            nickname = Nickname(DEFAULT_NAME),
-        ),
-        BoardData(
-            title = Title(DEFAULT_TITLE),
-            content = DEFAULT_CONTENT,
-            tags = Tags(),
-            nickname = Nickname(DEFAULT_NAME),
-        ),
-        BoardData(
-            title = Title(DEFAULT_TITLE),
-            tags = Tags(),
-            nickname = Nickname(DEFAULT_NAME),
-        ),
-        BoardData(
-            title = Title(MAX_TITLE),
-            content = MAX_CONTENT,
-            tags = Tags(listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임")),
-            nickname = Nickname(MAX_NAME),
-        ),
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun BoardScreenView(@PreviewParameter(BoardPreviewParameterProvider::class) board: BoardData) {
-    KanbanBoardTemplate(
-        title = board.title,
-        content = board.content,
-        tags = board.tags,
-        nickname = board.nickname,
-    )
 }
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanBoardTemplate.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanBoardTemplate.kt
@@ -10,8 +10,18 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import woowacourse.kanban.board.constant.BORDER_COLOR
+import woowacourse.kanban.board.constant.DEFAULT_CONTENT
+import woowacourse.kanban.board.constant.DEFAULT_NAME
+import woowacourse.kanban.board.constant.DEFAULT_TITLE
+import woowacourse.kanban.board.constant.MAX_CONTENT
+import woowacourse.kanban.board.constant.MAX_NAME
+import woowacourse.kanban.board.constant.MAX_TITLE
+import woowacourse.kanban.board.model.BoardData
 import woowacourse.kanban.board.model.Nickname
 import woowacourse.kanban.board.model.Tags
 import woowacourse.kanban.board.model.Title
@@ -49,4 +59,48 @@ fun KanbanBoardTemplate(title: Title, content: String = "", tags: Tags, nickname
             Profile(nickname = nickname, modifier = Modifier.padding(vertical = 8.dp))
         }
     }
+}
+
+class BoardPreviewParameterProvider : PreviewParameterProvider<BoardData> {
+    override val values = sequenceOf(
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            content = DEFAULT_CONTENT,
+            tags = Tags(listOf("컴포넌트", "성능")),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            tags = Tags(listOf("컴포넌트", "성능")),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            content = DEFAULT_CONTENT,
+            tags = Tags(),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(DEFAULT_TITLE),
+            tags = Tags(),
+            nickname = Nickname(DEFAULT_NAME),
+        ),
+        BoardData(
+            title = Title(MAX_TITLE),
+            content = MAX_CONTENT,
+            tags = Tags(listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임")),
+            nickname = Nickname(MAX_NAME),
+        ),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BoardScreenView(@PreviewParameter(BoardPreviewParameterProvider::class) board: BoardData) {
+    KanbanBoardTemplate(
+        title = board.title,
+        content = board.content,
+        tags = board.tags,
+        nickname = board.nickname,
+    )
 }
